f0663a249cae34b3db59c531beec2b46b28f54fc
KaedenOC <komeacarr2@gmail.com>
2023-05-01 14:45:04 -0700

    resources schema

---
+++ models/resources.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+
+const resourceSchema = new Schema({
+  title: {type: String, required: true},
+  description: {type: String, required: false},
+  url: {type: String, required: true},
+  email: {type: String}
+})
+
+const Resources = mongoose.model('resources', resourceSchema);
+
+module.exports = Resources;
