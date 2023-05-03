63fcf755dca6a668822183eb0bded13858da2945
Heather Holcomb <holcombheather@gmail.com>
2023-05-02 17:25:27 -0700

    updated backend auth functionality for todo, resources, calendar

--- models/todo.js
+++ models/todo.js
@@ -9,8 +9,9 @@ const oneDay = 24 * 60 * 60 * 1000; // 1 day in miliseconds
 const todoSchema = new Schema({
     task: {type: String, required: true},
     dueDate: {type: Date, required: false, default:() => Date.now() + oneDay},
-    completed: {type: Boolean, required: true, default:false}  
-})
+    completed: {type: Boolean, required: true, default:false},
+    email: String  
+});
 
 const ToDo = mongoose.model('todo', todoSchema);
 
